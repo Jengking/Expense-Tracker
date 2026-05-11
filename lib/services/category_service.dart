bfc9dfd3728250880e3b4b9fa948192a37661354
Fadzley <mfadzleyhud@yahoo.com>
2026-05-11 12:38:58 +0800

    load local json file

--- lib/services/category_service.dart
+++ lib/services/category_service.dart
@@ -1,22 +1,18 @@
 import 'dart:convert';
-import 'package:http/http.dart' as http;
+import 'package:flutter/services.dart';
 import '../models/category.dart';
 
 class CategoryService {
-  final String _url = "https://media.halogen.my/experiment/mobile/expenseCategories.json";
+  final String _assetPath = "assets/json/expenseCategories.json";
 
   Future<List<Category>> fetchCategories() async {
     try {
-      final response = await http.get(Uri.parse(_url));
-      if(response.statusCode == 200) {
-        Map<String, dynamic> jsonResponse = jsonDecode(response.body);
-        List<dynamic> expensesCategories = jsonResponse['expenseCategories'];
-        return expensesCategories
-            .map((dynamic item) => Category.fromJson(item as Map<String, dynamic>))
-            .toList();
-      } else {
-        throw Exception('Failed to load categories data: ${response.statusCode} - ${response.body}');
-      }
+      final response = await rootBundle.loadString(_assetPath);
+      Map<String, dynamic> jsonResponse = jsonDecode(response);
+      List<dynamic> expensesCategories = jsonResponse['expenseCategories'];
+      return expensesCategories
+          .map((dynamic item) => Category.fromJson(item as Map<String, dynamic>))
+          .toList();
     } catch(e) {
       print('Error fetching data: $e');
       throw Exception('Failed to fetch data: $e');
